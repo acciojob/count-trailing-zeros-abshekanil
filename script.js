b61f182ab9f1791640e489b181c3d147cce7165f
Abhishek Anil <141896050+abshekanil@users.noreply.github.com>
2024-01-09 11:19:35 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,17 +1,17 @@
 function trailingZeros(n) {
   //your JS code here. If required.
+	
+
 	let count = 0;
+
 	while(n > 0)
 		{
-			let ld = n%10;
-			if(ld === 0)
-			{
-				count++;
-			} 
-
-			n = Math.floor(n/10);
+			n = Math.floor(n / 5);
+			count++;
+			
 		}
-	return count;
+	return count;  
+	
 }
 
 const input = prompt("Enter a number");
